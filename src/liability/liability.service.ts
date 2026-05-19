d3b1e2bd74f6a415d8ccd46e57b3f3b8830f2dc0
Ahmad <ahmad@bansay.dev>
2026-05-19 14:52:00 +0800

    add CRUD message patterns to liability service

--- src/liability/liability.service.ts
+++ src/liability/liability.service.ts
@@ -1,5 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateLiabilityDto } from './dto/create-liability.dto';
+import { UpdateLiabilityDto } from './dto/update-liability.dto';
+import { QueryLiabilityDto } from './dto/query-liability.dto';
 
 @Injectable()
 export class LiabilityService {
@@ -7,5 +10,25 @@ export class LiabilityService {
 
   ping() {
     return this.client.send({ cmd: 'liability.ping' }, {});
+  }
+
+  create(dto: CreateLiabilityDto) {
+    return this.client.send({ cmd: 'liability.create' }, dto);
+  }
+
+  findAll(query: QueryLiabilityDto) {
+    return this.client.send({ cmd: 'liability.findAll' }, query);
+  }
+
+  findOne(id: number) {
+    return this.client.send({ cmd: 'liability.findOne' }, { id });
+  }
+
+  update(id: number, dto: UpdateLiabilityDto) {
+    return this.client.send({ cmd: 'liability.update' }, { id, ...dto });
+  }
+
+  remove(id: number) {
+    return this.client.send({ cmd: 'liability.remove' }, { id });
   }
 }
