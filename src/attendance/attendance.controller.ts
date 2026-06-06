85eca2b34d9282fbb1944c6b29ddc2bb44bb6281
MantokKa <imselfno@gmail.com>
2026-06-06 15:49:15 +0800

    docs(swagger): add swagger annotations to attendance controller

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Attendance')
 @Controller('attendance')
 export class AttendanceController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
   @Get('ping')
+  @ApiOperation({ summary: 'Ping the Attendance microservice' })
+  @ApiResponse({ status: 200, description: 'Microservice is reachable' })
   async ping() {
     return this.attendanceService.ping();
   }
 }
+
