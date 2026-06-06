b90c4a28926e35d8beb3a8541050d2b2393971bc
MantokKa <imselfno@gmail.com>
2026-06-06 15:49:33 +0800

    docs(swagger): add swagger annotations to user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('ping')
+  @ApiOperation({ summary: 'Ping the User microservice' })
+  @ApiResponse({ status: 200, description: 'Microservice is reachable' })
   async ping() {
     return this.userService.ping();
   }
 }
+
