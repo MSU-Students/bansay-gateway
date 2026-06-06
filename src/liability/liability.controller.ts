23ed0a9da10a19dbc040b75bce019a19ec06ca6a
MantokKa <imselfno@gmail.com>
2026-06-06 15:49:24 +0800

    docs(swagger): add swagger annotations to liability controller

--- src/liability/liability.controller.ts
+++ src/liability/liability.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { LiabilityService } from './liability.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Liability')
 @Controller('liability')
 export class LiabilityController {
   constructor(private readonly liabiityService: LiabilityService) {}
 
   @Get('ping')
+  @ApiOperation({ summary: 'Ping the Liability microservice' })
+  @ApiResponse({ status: 200, description: 'Microservice is reachable' })
   async ping() {
     return this.liabiityService.ping();
   }
 }
+
